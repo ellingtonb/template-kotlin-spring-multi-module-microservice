bc5ddf143cb6dd4dc56684ff2bc587b87d6c0df0
Ellington Brambila <contato@ellingtonb.com>
2022-07-01 20:43:23 -0300

    Added spring component scan to main application

--- modules/webservice/src/main/kotlin/com/ellingtonb/webservice/WebserviceApplication.kt
+++ modules/webservice/src/main/kotlin/com/ellingtonb/webservice/WebserviceApplication.kt
@@ -2,8 +2,12 @@ package com.ellingtonb.webservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan(
+    value = ["com.ellingtonb"]
+)
 class WebserviceApplication
 
 fun main(args: Array<String>) {
